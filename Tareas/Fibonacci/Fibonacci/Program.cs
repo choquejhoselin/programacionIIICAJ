4b4d834c90edeec8ab037c0bee83e2b0fa38139f
choquejhoselin <choquejhoselin84@gmail.com>
2020-03-23 15:15:42 -0400

    Fibonacci

---
+++ Tareas/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        private static readonly int n;
+
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Numero fibonacci");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("El fibonacci recursivo es" + FibonacciRecursivo(numero));
+
+            Console.ReadKey();
+        }
+
+        static int FibonacciRecursivo(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return FibonacciRecursivo(n - 1) + FibonacciRecursivo(n - 2);
+        }
+
+    }
+}
